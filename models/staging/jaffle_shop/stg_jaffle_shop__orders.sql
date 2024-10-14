fb7de99f84de7ed7a976a2ccdda953fede903044
Olha Polishchuk <polischuk280603@gmail.com>
2024-10-14 08:48:18 +0000

    Sources

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,7 @@
 select
-    id as order_id,
-    user_id as customer_id,
+   id as  order_id,
+   user_id as customer_id,
     order_date,
     status
 
-from default.jaffle_shop_orders
+from {{source('jaffle_shop_orders','jaffle_shop_orders')}}
